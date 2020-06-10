f443e59c98dd3ca88e13b165e71a690e04452d02
Barbora Dejlova <barboradejlova@BarborasMacBook.fios-router.home>
2020-06-10 17:00:01 -0400

    finally done

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -11,29 +11,29 @@ import "./Posts.css";
 
 const Post = props => {
   // set up state for the likes
-  const [likes, setLikes] = useState(props.post.likes);
+  const [likes, setLikes] = useState(props.dataFromParent.likes);
   const addLikes = evt => {
     setLikes(likes + 1)
   };
   return (
     <div className="post-border">
       <PostHeader
-        username={props.post.username}
+        username={props.dataFromParent.username}
         thumbnailUrl={
-          props.post.thumbnailUrl
+          props.dataFromParent.thumbnailUrl
         }
       />
       <div className="post-image-wrapper">
         <img
           alt="post thumbnail"
           className="post-image"
-          src={props.post.imageUrl}
+          src={props.dataFromParent.imageUrl}
         />
       </div>
       <LikeSection likes={likes} setLikes={addLikes} />
       <CommentSection
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
+        postId={props.dataFromParent.imageUrl}
+        comments={props.dataFromParent.comments}
       />
     </div>
   );
